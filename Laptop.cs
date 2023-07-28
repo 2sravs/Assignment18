1d8ac85fe65586401903ee4612944f1b668aff7b
2Sravs <sravani.chekuri1243#gmail.com>
2023-07-28 15:35:17 +0530

    firstcommit

---
+++ Laptop.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment18
+{
+    public class Laptop : IConnectable, Irechargeble, IDisplayable
+    {
+        public string Model { get; set; }
+        public bool IsConnected { get; private set; }
+        public int BatteryLevel { get; private set; }
+
+        public Laptop(string model)
+        {
+            Model = model;
+            BatteryLevel = 100;
+        }
+
+        public bool Connect()
+        {
+            Console.WriteLine($"Connecting {Model}...");
+            IsConnected = true;
+            return IsConnected;
+        }
+
+        public void Charge(int minutes)
+        {
+            Console.WriteLine($"Charging {Model} for {minutes} minutes...");
+            BatteryLevel += minutes / 15;
+            BatteryLevel = Math.Min(BatteryLevel, 100);
+        }
+
+        public string Display()
+        {
+            return $"Laptop Model: {Model}, Battery Level: {BatteryLevel}%";
+        }
+
+        public bool connect()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
